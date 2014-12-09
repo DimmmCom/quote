4880aaab777ddc20f26cfb5d6d6de2ff35893a88
DimmmCom <dimmmcom@yandex.ru>
2014-12-09 23:19:24 +0300

    Initial commit

---
+++ ext.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*
+* @package Quote
+* @copyright (c) 2014 DimmmCom
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace dimmmcom\quote;
+
+/**
+* Main extension class for this extension.
+*/
+class ext extends \phpbb\extension\base
+{
+}
